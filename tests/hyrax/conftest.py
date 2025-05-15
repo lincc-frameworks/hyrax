7f72349ef20aa2c33314cff505725f0eabc867d4
Michael Tauraso <mtauraso@users.noreply.github.com>
2025-05-15 14:49:21 -0700

    Add umap ordering tests and use pytest-xdist (#322)
    * Added pytest-xdist to our standard pre-commit. shaves ~6s off a
      pytest run. After pip installing dev dependencies, run
      'pytest -n auto -m "not slow"' to run parallelized.
    * Added a Umap ordering/data identity test
    * Factored more of our fixtures for infer/index/umap tests
      into conftest.py
    * Removed need to `.train()` in shared fixtures to speed up tests
    * Added test of train to avoid lowering coverage %

--- tests/hyrax/conftest.py
+++ tests/hyrax/conftest.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pytest
 import torch.nn as nn
 from torch import from_numpy
 from torch.utils.data import Dataset, IterableDataset
 
+import hyrax
 from hyrax.data_sets import HyraxDataset
 from hyrax.models import hyrax_model
 
@@ -12,10 +14,21 @@ class LoopbackModel(nn.Module):
     """Simple model for testing which returns its own input"""
 
     def __init__(self, config, shape):
+        from functools import partial
+
         super().__init__()
         # This is created so the optimizier can find at least one weight
         self.unused_module = nn.Conv2d(1, 1, kernel_size=1, stride=0, padding=0)
         self.config = config
+
+        def load(self, weight_file):
+            """Load Weights, we have no weights so we do nothing"""
+            pass
+
+        # We override this way rather than defining a method because
+        # Torch has some __init__ related cleverness which stomps our
+        # load definition when performed in the usual fashion.
+        self.load = partial(load, self)
 
     def forward(self, x):
         """We simply return our input"""
@@ -60,3 +73,47 @@ class RandomIterableDataset(RandomDataset, IterableDataset):
     def __iter__(self):
         for item in self.data:
             yield from_numpy(item)
+
+
+@pytest.fixture(scope="function", params=["RandomDataset", "RandomIterableDataset"])
+def loopback_hyrax(tmp_path_factory, request):
+    """This generates a loopback hyrax instance
+    which is configured to use the loopback model
+    and a simple dataset yielding random numbers
+    """
+    results_dir = tmp_path_factory.mktemp(f"loopback_hyrax_{request.param}")
+
+    h = hyrax.Hyrax()
+    h.config["model"]["name"] = "LoopbackModel"
+    h.config["train"]["epochs"] = 1
+    h.config["data_loader"]["batch_size"] = 5
+    h.config["general"]["results_dir"] = str(results_dir)
+
+    h.config["general"]["dev_mode"] = True
+    h.config["data_set"]["name"] = request.param
+    h.config["data_set"]["size"] = 20
+    h.config["data_set"]["seed"] = 0
+
+    h.config["data_set"]["validate_size"] = 0.2
+    h.config["data_set"]["test_size"] = 0.2
+    h.config["data_set"]["train_size"] = 0.6
+
+    weights_file = results_dir / "fakeweights"
+    with open(weights_file, "a"):
+        pass
+    h.config["infer"]["model_weights_file"] = str(weights_file)
+
+    dataset = h.prepare()
+    return h, dataset
+
+
+@pytest.fixture(scope="function")
+def loopback_inferred_hyrax(loopback_hyrax):
+    """This generates a loopback hyrax instance which is configured to use the
+    loopback model and a simple dataset yielding random numbers. It includes a call
+    to hyrax.infer which will produce the output consumed by vdb_index or umap."""
+
+    h, dataset = loopback_hyrax
+    inference_results = h.infer()
+
+    return h, dataset, inference_results
