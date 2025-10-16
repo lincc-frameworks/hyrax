23eb193f0212c3bc26e639dea0e73290dc882527
Drew Oldag <47493171+drewoldag@users.noreply.github.com>
2025-10-16 11:02:58 -0700

    Attach final metrics to model returned from `h.train()`. (#485)
    * Removing unnecessary calls to `to_tensor` in autoencoder models. Clean up naming from `data_set` to `dataset` in pytorch-ignite. Attach final metrics to the model output from `h.train()`.
    
    * Adding final validation metrics to the model.

--- src/hyrax/models/hyrax_autoencoder.py
+++ src/hyrax/models/hyrax_autoencoder.py
@@ -28,7 +28,7 @@ class HyraxAutoencoder(nn.Module):
         super().__init__()
         self.config = config
 
-        shape = self.to_tensor(data_sample).shape
+        shape = data_sample.shape
         logger.debug(f"Found shape: {shape} in data sample, using this to initialize model.")
 
         self.num_input_channels, self.image_width, self.image_height = shape
