eabfb59388a176bab897c12c9c476a38c401416b
Derek Jones <dtj1s@uw.edu>
2026-01-29 15:09:23 -0800

    Remove dangling references to dataset validators

--- src/hyrax/config_schemas/__init__.py
+++ src/hyrax/config_schemas/__init__.py
@@ -8,23 +8,9 @@ incremental adoption in downstream modules and tests.
 
 from .base import BaseConfigModel
 from .data_request import DataRequestConfig, DataRequestDefinition
-from .datasets import (
-    DownloadedLSSTDatasetConfig,
-    HSCDataSetConfig,
-    HyraxCifarDatasetConfig,
-    HyraxCSVDatasetConfig,
-    HyraxRandomDatasetConfig,
-    LSSTDatasetConfig,
-)
 
 __all__ = [
     "BaseConfigModel",
     "DataRequestConfig",
     "DataRequestDefinition",
-    "HyraxRandomDatasetConfig",
-    "HyraxCifarDatasetConfig",
-    "LSSTDatasetConfig",
-    "DownloadedLSSTDatasetConfig",
-    "HSCDataSetConfig",
-    "HyraxCSVDatasetConfig",
 ]
