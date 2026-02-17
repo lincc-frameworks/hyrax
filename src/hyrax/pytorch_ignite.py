4c93171b07dc35baa1fd918cc2a12594e3febc79
Michael Tauraso <mtauraso@users.noreply.github.com>
2026-02-17 10:47:41 -0800

    Use the scheduler on the inner model, not the wrapped model (#706)

--- src/hyrax/pytorch_ignite.py
+++ src/hyrax/pytorch_ignite.py
@@ -731,14 +731,14 @@ def create_trainer(model: torch.nn.Module, config: dict, results_directory: Path
 
     @trainer.on(HyraxEvents.HYRAX_EPOCH_COMPLETED)
     def scheduler_step(trainer):
-        if model.scheduler:
+        if scheduler:
             if not hasattr(model, "_learning_rates_history"):
                 model._learning_rates_history = []
-            epoch_lr = model.scheduler.get_last_lr()
+            epoch_lr = scheduler.get_last_lr()
             epoch_number = trainer.state.epoch - 1
             model._learning_rates_history.append(epoch_lr)
             tensorboardx_logger.add_scalar("training/training/epoch/lr", epoch_lr, global_step=epoch_number)
-            model.scheduler.step()
+            scheduler.step()
 
     trainer.add_event_handler(HyraxEvents.HYRAX_EPOCH_COMPLETED, latest_checkpoint)
     trainer.add_event_handler(HyraxEvents.HYRAX_EPOCH_COMPLETED, best_checkpoint)
