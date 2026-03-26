3003f6939f53adfe8f3f472f2275d6d400428bea
Samarth Venkatesh <samnsid7@uw.edu>
2026-03-26 13:46:03 -0700

    ran linter

--- src/hyrax/verbs/prepare.py
+++ src/hyrax/verbs/prepare.py
@@ -10,10 +10,10 @@ logger = logging.getLogger(__name__)
 @hyrax_verb
 class Prepare(Verb):
     """Prepare verb"""
-    
+
     cli_name = "prepare"
     add_parser_kwargs = {}
-    
+
     @staticmethod
     def setup_parser(parser):
         """We don't need any parser setup for CLI opts"""
