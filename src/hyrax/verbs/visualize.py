bb31a571def014d71f0be0a96774661ce1d8b7ab
Max West <maxwest@uw.edu>
2026-06-08 11:18:05 -0700

    simplify and fix some linting issues

--- src/hyrax/verbs/visualize.py
+++ src/hyrax/verbs/visualize.py
@@ -116,12 +116,7 @@ class Visualize(Verb):
 
         # Get the umap data and put it in a kdtree for indexing.
         self.umap_results = load_results_dataset(self.config, results_dir=input_dir, verb="umap")
-        self.umap_results = np.vstack(
-                [
-                    np.concatenate([row[key].flatten() for key in self.umap_results])
-                    for row in self.umap_results
-                ]
-        )
+        self.umap_results = self.umap_results.get_combined_tensor(np.arange(len(self.umap_results)))
 
         # Build a DataProvider from the live config for metadata access.
         # This avoids implicit coupling between result datasets and their original data sources.
