3f36adc8b1420aeaf08253840ab0874a1e0cfe29
Gracia Wang <wqy37@uw.edu>
2026-06-22 16:49:20 -0700

    format fix

--- src/hyrax/config_migrations/migrations/004_move_umap_to_reduce.py
+++ src/hyrax/config_migrations/migrations/004_move_umap_to_reduce.py
@@ -11,14 +11,14 @@ from hyrax.config_migrations.migration_utils import migration_step, move_key
 
 
 @migration_step(
-        from_version=4, 
-        key_renames={
-            "umap.fit_sample_size": "reduce.umap.fit_sample_size",
-            "umap.model_path": "reduce.umap.model_path",
-            "umap.save_fit_umap": "reduce.save_fit_model",
-            "umap.parallel": "reduce.parallel",
-            "umap.UMAP": "reduce.umap.kwargs",
-        }
+    from_version=4,
+    key_renames={
+        "umap.fit_sample_size": "reduce.umap.fit_sample_size",
+        "umap.model_path": "reduce.umap.model_path",
+        "umap.save_fit_umap": "reduce.save_fit_model",
+        "umap.parallel": "reduce.parallel",
+        "umap.UMAP": "reduce.umap.kwargs",
+    },
 )
 def move_umap_to_reduce(cfg: TOMLDocument) -> TOMLDocument:
     """Move the legacy ``[umap]`` and ``[umap.UMAP]`` to be under ``[reduce]``."""
@@ -49,13 +49,13 @@ def move_umap_to_reduce(cfg: TOMLDocument) -> TOMLDocument:
     move_key(cfg, "umap.parallel", "reduce.parallel")
     if "name" in umap_tbl and umap_tbl["name"] == "umap.UMAP":
         reduce_tbl["algorithm"] = "umap"
-    
+
     # Move umap.UMAP kwargs to reduce.umap.kwargs
     move_key(cfg, "umap.UMAP", "reduce.umap.kwargs")
 
     # Delete the old umap section
     del cfg["umap"]
-    
+
     # Adding tsne section
     reduce_tbl["tsne"] = tomlkit.table()
 
