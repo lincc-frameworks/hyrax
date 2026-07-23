e3cd7bc312df12c1f36db322d188d6ef8675f8dd
Gracia Wang <wqy37@uw.edu>
2026-07-23 11:52:14 -0700

    test asv track_ function and update timebench file name

--- benchmarks/time_pipeline_benchmarks.py
+++ benchmarks/time_pipeline_benchmarks.py
@@ -20,6 +20,14 @@ CONFIG = {
 }
 
 
+def track_test():
+    """This test function should be able to detected by asv and run along for saving results"""
+    if Path("./gondor_results").is_dir():
+        return(10000000000000)
+    else:
+        return(99999999999999)
+
+
 def get_device():
     if torch.backends.mps.is_available():
         return "mps"
