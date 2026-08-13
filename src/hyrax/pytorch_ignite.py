962e87ab1e1bc28c2521e9ef5ad0cadfa607398e
Drew Oldag <47493171+drewoldag@users.noreply.github.com>
2026-08-13 14:23:54 -0700

    Adding validation post-epoch hook. (#1030)
    * Adding validation post-epoch hook.
    
    * Fixing function name.

--- src/hyrax/pytorch_ignite.py
+++ src/hyrax/pytorch_ignite.py
@@ -542,6 +542,15 @@ def create_validator(
         logger.debug(f"Validation metrics: {validator.state.output}")
         model.final_validation_metrics = validator.state.output
 
+    @validator.on(HyraxEvents.HYRAX_EPOCH_COMPLETED)
+    def run_validate_post_epoch_hooks(validator):
+        hook = getattr(model, "validate_post_epoch", None)
+        if not callable(hook):
+            return
+        if idist.get_world_size() > 1:
+            raise NotImplementedError("validate_post_epoch is not supported for distributed training yet.")
+        hook()
+
     @trainer.on(HyraxEvents.HYRAX_EPOCH_COMPLETED)
     def run_validation():
         with torch.no_grad():
