650ebfa597b1e27f646e1ae767bdddc5de64d063
Drew Oldag <47493171+drewoldag@users.noreply.github.com>
2026-09-23 09:18:57 -0700

    Apply batched suggestions from code review
    Co-authored-by: Drew Oldag <47493171+drewoldag@users.noreply.github.com>

--- src/hyrax/datasets/lsdb_stream_dataset.py
+++ src/hyrax/datasets/lsdb_stream_dataset.py
@@ -345,6 +345,7 @@ class LSDBStreamDataset(HyraxDataset, IterableDataset):
                     f"Connecting the lsdb stream to the dask scheduler at {self.dask_client_address}."
                 )
                 self._owned_client = Client(self.dask_client_address)
+                logger.info(f"Dask client dashboard: {self._owned_client.dashboard_link}") 
             return self._owned_client
 
         try:
@@ -535,6 +536,8 @@ class LSDBStreamDataset(HyraxDataset, IterableDataset):
                     break
 
                 frame = chunk
+                logger.debug(f"Most recent chunk size: {len(chunk)}")
+                logger.debug(f"Will produce {np.floor(len(chunk)/self.batch_size)} batches of size {self._batch_size}")  
                 taken, n_rows = 0, len(frame)
                 while taken < n_rows:
                     need = self.batch_size - len(batch)
